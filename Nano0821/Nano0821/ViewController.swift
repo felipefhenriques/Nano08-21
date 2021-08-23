be8db9d3adb1478dc9a7af492933afe427ee0e60
Felipe Henriques <felipeferreirahenriques@outlook.com>
2021-08-23 13:38:41 -0300

    projeto view code com configurações iniciais

---
+++ Nano0821/Nano0821/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nano0821
+//
+//  Created by Felipe Ferreira on 23/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .cyan
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Nano0821/Nano0821Tests/Nano0821Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Nano0821Tests.swift
+//  Nano0821Tests
+//
+//  Created by Felipe Ferreira on 23/08/21.
+//
+
+import XCTest
+@testable import Nano0821
+
+class Nano0821Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
