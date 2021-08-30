9cb7ddad6b22add2ced7c701569e3721519bc30a
Felipe Henriques <felipeferreirahenriques@outlook.com>
2021-08-30 08:35:45 -0300

    imagem e data de nascimento incorporadas no app

--- Nano0821/Nano0821/Model/PetCoreData.swift
+++ Nano0821/Nano0821/Model/PetCoreData.swift
@@ -30,6 +30,7 @@ class petCoreData {
             managedObj.setValue(0, forKey: "index")
         }
         
+        managedObj.setValue(pet.imgData, forKey: "imagem")
         managedObj.setValue(pet.especie, forKey: "especie")
         managedObj.setValue(pet.nome, forKey: "nome")
         managedObj.setValue(pet.raca, forKey: "raca")
@@ -37,6 +38,10 @@ class petCoreData {
         managedObj.setValue(pet.cor, forKey: "cor")
         managedObj.setValue(pet.peso, forKey: "peso")
         managedObj.setValue(pet.outros, forKey: "outros")
+        
+        managedObj.setValue(pet.ano, forKey: "anoNasc")
+        managedObj.setValue(pet.mes, forKey: "mesNasc")
+        managedObj.setValue(pet.dia, forKey: "diaNasc")
         
         do {
             try objectContext.save()
@@ -79,17 +84,22 @@ class petCoreData {
             print("Não foi possível carregar os dados. \(error), \(error.userInfo)")
             }
         
+        
         if savedPets.count > 0 {
             for i in 0...savedPets.count-1 {
                 let pet: Pet = Pet(
                     index: savedPets[i].value(forKey: "index") as! Int,
+                    imgData: savedPets[i].value(forKey: "imagem") as! Data,
                     especie: savedPets[i].value(forKey: "especie") as! String,
                     nome: savedPets[i].value(forKey: "nome") as! String,
                     raca: savedPets[i].value(forKey: "raca") as! String,
                     sexo: savedPets[i].value(forKey: "sexo") as! String,
                     cor: savedPets[i].value(forKey: "cor") as! String,
                     peso: savedPets[i].value(forKey: "peso") as! Double,
-                    outros: savedPets[i].value(forKey: "outros") as! String)
+                    outros: savedPets[i].value(forKey: "outros") as! String,
+                    dia: savedPets[i].value(forKey: "diaNasc") as! String,
+                    mes: savedPets[i].value(forKey: "mesNasc") as! String,
+                    ano: savedPets[i].value(forKey: "anoNasc") as! String)
                 petsArray.append(pet)
             }
         }
